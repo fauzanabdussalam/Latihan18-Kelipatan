37f806677257d7934b07fa04cbb32637c9ddf894
fauzan_abdussalam <fauzan@3trust.com>
2020-10-31 03:20:12 +0700

    -

---
+++ src/pbo10k/pkg10119901/latihan18/kelipatan/PBO10K10119901Latihan18Kelipatan.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo10k.pkg10119901.latihan18.kelipatan;
+
+/**
+ *
+ * @author
+ * NAMA     : Fauzan Muhammad Abdussalam
+ * KELAS    : IF-10K
+ * NIM      : 10119901
+ * Deskripsi Program : Menghitung Kelipatan
+ *
+ */
+
+public class PBO10K10119901Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double i = 3.5;
+
+        while (i <= 35) 
+        {
+            System.out.println(i);
+
+            i += 3.5;
+        }
+    }
+}
